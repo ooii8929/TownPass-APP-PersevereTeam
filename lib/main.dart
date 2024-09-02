848f446faad5f0dd379fe4f8d6b030099c3e62f0
Alvin Lin <alvinlin@kkcompany.com>
2024-09-02 20:18:14 +0800

    feat: add speech to text dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,16 @@ import 'package:town_pass/service/package_service.dart';
 import 'package:town_pass/util/tp_colors.dart';
 import 'package:town_pass/util/tp_route.dart';
 import 'package:town_pass/service/shared_preferences_service.dart';
-import 'package:town_pass/service/speech_to_text_service.dart'; // 导入 WifiInfoService
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
-import 'dart:developer' as developer;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // FlutterNativeSplash.preserve(
   //   widgetsBinding: WidgetsFlutterBinding.ensureInitialized(),
   // );
-
-  developer.log('Application started'); // 添加日志记录
 
   await initServices();
 
@@ -29,23 +25,10 @@ void main() async {
     ),
   );
 
-  // 初始化 WifiInfoService 并打印 WiFi 信息
-  developer.log('Initializing WifiInfoService'); // 添加日志记录
-  final wifiInfoService = await Get.putAsync(() => WifiInfoService().init());
-  developer.log('WiFiInfoService initialized'); // 添加日志记录
-  developer.log('WiFi Name: ${wifiInfoService.wifiName}');
-  developer.log('WiFi BSSID: ${wifiInfoService.wifiBSSID}');
-  developer.log('WiFi IPv4: ${wifiInfoService.wifiIPv4}');
-  developer.log('WiFi IPv6: ${wifiInfoService.wifiIPv6}');
-  developer.log('WiFi Gateway IP: ${wifiInfoService.wifiGatewayIP}');
-  developer.log('WiFi Broadcast: ${wifiInfoService.wifiBroadcast}');
-  developer.log('WiFi Submask: ${wifiInfoService.wifiSubmask}');
-
   runApp(const MyApp());
 }
 
 Future<void> initServices() async {
-  developer.log('Initializing services'); // 添加日志记录
   await Get.putAsync<AccountService>(() async => await AccountService().init());
   await Get.putAsync<DeviceService>(() async => await DeviceService().init());
   await Get.putAsync<PackageService>(() async => await PackageService().init());
@@ -53,7 +36,6 @@ Future<void> initServices() async {
       () async => await SharedPreferencesService().init());
   await Get.putAsync<GeoLocatorService>(
       () async => await GeoLocatorService().init());
-  developer.log('Services initialized'); // 添加日志记录
 }
 
 class MyApp extends StatelessWidget {
@@ -61,7 +43,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    developer.log('Building MyApp widget'); // 添加日志记录
     return GetMaterialApp(
       title: 'City Pass',
       theme: ThemeData(
